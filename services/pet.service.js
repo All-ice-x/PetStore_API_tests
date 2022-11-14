7ec35ed6b49bd164accaffba535f6708cf6668d7
Alice <snakedy.alice@gmail.com>
2022-11-14 21:44:33 +0300

    add UpdatePet, DeletePet methods; fix swagger

--- services/pet.service.js
+++ services/pet.service.js
@@ -4,25 +4,28 @@ import { urls } from '../config/index';
 import { loadApiSpec, validate } from '../lib/validator';
 
 const Pet = {
-  findByStatus: async (status) => {
+  findByStatus: async (status) => { // поиск питомца по статусу
     const response = await supertest(urls.petstore)
       .get(`/pet/findByStatus${status}`)
+      .set('Accept', 'application/json')
       .set('Content-Type', 'application/json');
     allure.attachment('response', JSON.stringify(response.body), 'application/json');
 
     const apiSpec = await loadApiSpec('./lib/swagger.json');
-    const schema = apiSpec.paths['/pet/findByTags'].get.responses[200];
+    const schema = apiSpec.paths['/pet/findByStatus'].get.responses[200];
     validate(schema, response.body);
+
     return response;
   },
 
-  addANewPet: async (body) => {
+  addANewPet: async (body) => { // добавление нового питомца в каталог
     const response = await supertest(urls.petstore)
       .post('/pet')
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
       .send(body);
     allure.attachment('response', JSON.stringify(response.body), 'application/json');
+
     const apiSpec = await loadApiSpec('./lib/swagger.json');
     const schema = apiSpec.paths['/pet'].post.responses[200];
     validate(schema, response.body);
@@ -30,15 +33,47 @@ const Pet = {
     return response;
   },
 
-  findByPetId: async (id) => {
+  findByPetId: async (id) => { // поиск питомца по ID
     const response = await supertest(urls.petstore)
       .get(`/pet/${id}`)
+      .set('Accept', 'application/json')
       .set('Content-Type', 'application/json');
     allure.attachment('response', JSON.stringify(response.body), 'application/json');
 
     const apiSpec = await loadApiSpec('./lib/swagger.json');
-    const schema = apiSpec.paths['/pet/findByTags'].get.responses[200];
+    const schema = apiSpec.paths['/pet/{petId}'].get.responses[200];
     validate(schema, response.body);
+
+    return response;
+  },
+
+  UpdatePet: async (body) => { //  обновление информации о питомце
+    const response = await supertest(urls.petstore)
+      .put('/pet')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send(body);
+    allure.attachment('response', JSON.stringify(response.body), 'application/json');
+
+    const apiSpec = await loadApiSpec('./lib/swagger.json');
+    const schema = apiSpec.paths['/pet'].put.responses[200];
+    validate(schema, response.body);
+
+    return response;
+  },
+
+  DeletePet: async (id) => { //  удаление питомца из каталога
+    const response = await supertest(urls.petstore)
+      .delete(`/pet/${id}`)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send(id);
+    allure.attachment('response', JSON.stringify(response.body), 'application/json');
+
+    const apiSpec = await loadApiSpec('./lib/swagger.json');
+    const schema = apiSpec.paths['/pet/{petId}'].delete.responses[200];
+    validate(schema, response.body);
+
     return response;
   },
 };
--- spesc/petstore.spec.js
+++ spesc/petstore.spec.js
@@ -9,21 +9,19 @@ describe('Тестирование магазина питомцев', () => {
     const status = '?status=available';
     const r = await api().Pet().findByStatus(status);
     assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
-    console.log(r.body);
   });
 
   it('#02 - Получить список питомцев в обработке, 200', async () => {
     const status = '?status=pending';
     const r = await api().Pet().findByStatus(status);
     assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
-    console.log(r.body);
   });
 
   it('#03 - Получить список проданных питомцев, 200', async () => {
     const status = '?status=sold';
     const r = await api().Pet().findByStatus(status);
     assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
-    console.log(r.body);
+    // console.log(r.body);
   });
 
   it('#04 - Создать питомца с фото, 200', async () => {
@@ -48,81 +46,44 @@ describe('Тестирование магазина питомцев', () => {
     assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
     console.log(r.body);
   });
+
+  it('#06 - Обновить информацию о питомце, 200', async () => {
+    let body = new PetBuilder().setId().setCategory().setName()
+      .setPhoto()
+      .setTag()
+      .setStatus()
+      .build();
+    let r = await api().Pet().addANewPet(body);
+    console.log(r.body);
+    console.log('-------- U P D A T I N G --------');
+    r = await api().Pet().findByPetId(r.body.id);
+    body = {
+      id: r.body.id,
+      category: { id: r.body.category.id, name: r.body.category.name },
+      name: r.body.name,
+      photoUrls: r.body.photoUrls,
+      tags: [
+        { id: 1, name: '#cat_at_home' },
+        { id: 2, name: '#happy_cat' },
+      ],
+      status: 'sold',
+    };
+    r = await api().Pet().UpdatePet(body);
+    assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
+    console.log(r.body);
+  });
+
+  it('#07 - Удалить питомца по ID, 200', async () => {
+    const body = new PetBuilder().setId().setCategory().setName()
+      .setPhoto()
+      .setTag()
+      .setStatus()
+      .build();
+    let r = await api().Pet().addANewPet(body);
+    console.log(r.body);
+    console.log('-------- D E L E T I N G --------');
+    r = await api().Pet().DeletePet(r.body.id);
+    assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
+    console.log(r.body);
+  });
 });
-
-/*
-
-  // 05
-  it('#05 - Получить todo по ID, 200', async () => {
-    const body = new TodoBuilder().setName().setDescription().setDoneStatus(false)
-      .build();
-    let r = await api().Todos().post(body, token);
-    r = await api().TodosId().get(r.body.id, token);
-    assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
-  });
-
-  // 08
-  it('#08 - Создать todo, 201', async () => {
-    const body = new TodoBuilder().setName().setDescription().setDoneStatus(true)
-      .build();
-    const r = await api().Todos().post(body, token);
-    assert.strictEqual(r.statusCode, 201, 'statusCode не 201');
-  });
-
-  // 09
-  it('#09 - Отфильтровать todos по doneStatus=true, 200', async () => {
-    const status = '?doneStatus=true';
-    const r = await api().Todos().status(token, status);
-    assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
-  });
-
-  // 10
-  it('#10 - Получить ошибку по doneStatus на POST todo, 400', async () => {
-    const body = new TodoBuilder().setName().setDescription().setDoneStatus('ok')
-      .build();
-    const r = await api().Todos().post(body, token);
-    assert.strictEqual(r.statusCode, 400, 'statusCode не 400');
-  });
-
-  // 11
-  it('#11 - Обновить данные в todos/{id}, 200', async () => {
-    const body = new TodoBuilder().setName().setDescription().setDoneStatus(true)
-      .build();
-    let r = await api().Todos().post(body, token);
-    r = await api().TodosId().post(r.body.id, body, token);
-    assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
-  });
-
-  // 12
-  it('#12 - Удалить запись todos/{id}, 200', async () => {
-    const body = new TodoBuilder().setName().setDescription().setDoneStatus(true)
-      .build();
-    let r = await api().Todos().post(body, token);
-    r = await api().TodosId().delete(r.body.id, token);
-    assert.strictEqual(r.statusCode, 200, 'statusCode не 200');
-  });
-
-  // 21
-  it('#21 - Создать todo (формат body JSON), 201', async () => {
-    const body = new TodoBuilder().setName().setDescription().setDoneStatus(true)
-      .build();
-    const r = await api().Todos().post(body, token);
-    assert.strictEqual(r.statusCode, 201, 'statusCode не 201');
-  });
-
-  // 32
-  it('#32 - Получить ошибку "401 Unauthorized" в GET /secret/note, 401', async () => {
-    const r = await api().SecretNote().getNoAuth(token);
-    assert.strictEqual(r.statusCode, 401, 'statusCode не 401');
-  });
-
-  it('#39 - Удалить ВСЕ записи todos/{id}, 200', async () => {
-    let r = await api().Todos().get(token);
-    r.body.todos.forEach((item) => {
-      api().TodosId().delete(item.id, token);
-    });
-    r = await api().Todos().get(token);
-    assert.isEmpty(r.body.todos, 'Есть неудалённые todo');
-  });
-
-*/
